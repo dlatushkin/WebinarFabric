f6eab0dcc4ac88a5308218d9cab19d1b26f0b867
dlatushkin <dxwizard@gmail.com>
2020-06-04 13:38:41 +0300

    emulator: + suave

--- RailwayEmulator/RailwayEmulator/Program.fs
+++ RailwayEmulator/RailwayEmulator/Program.fs
@@ -1,8 +1,94 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Suave.ServerErrors
+open Suave.Writers
+open Newtonsoft.Json
+
+type NewAnswer =
+  {
+    Text: string
+  }
+
+type Answer =
+  {
+    Text: string
+    AnswerId: int
+  }
+
+let getString(rawForm: byte[]) =
+  System.Text.Encoding.UTF8.GetString(rawForm)
+
+let fromJson<'a> json =
+  JsonConvert.DeserializeObject(json, typeof<'a>) :?> 'a
+
+let getAnswerFromDb id =
+  { Text = "Sample Text"; AnswerId = id }
+
+let createAnswerDb (newAnswer: NewAnswer) =
+  { Text = newAnswer.Text; AnswerId = 10 }
+
+let updateAnswerDb answer =
+  answer
+
+let deleteAnswerFromDb id = 
+  true
+
+let getAnswer id =
+  getAnswerFromDb id
+  |> JsonConvert.SerializeObject
+  |> OK
+  >=> setMimeType "application/json"
+
+let createAnswer =
+  request(fun r ->
+    r.rawForm
+    |> getString
+    |> fromJson<NewAnswer>
+    |> createAnswerDb
+    |> JsonConvert.SerializeObject
+    |> CREATED)
+  >=> setMimeType "application/json"
+
+let updateAnswer =
+  request(fun r ->
+    r.rawForm
+    |> getString
+    |> fromJson<Answer>
+    |> updateAnswerDb
+    |> JsonConvert.SerializeObject
+    |> OK)
+  >=> setMimeType "application/json"
+
+let deleteAnswer id =
+  let successful = deleteAnswerFromDb id
+  if successful then
+    NO_CONTENT
+  else
+    INTERNAL_ERROR "Couldn't delete resource"
+
+let app = 
+  choose
+    [ GET >=> choose
+        [ path "/" >=> OK "Hello World"
+          pathScan "/answer/%d" (fun id -> getAnswer id) ]
+      POST >=> choose
+        [ path "/answer" >=> createAnswer ]
+      PUT >=> choose
+        [ path "/answer" >=> updateAnswer ]
+      DELETE >=> choose
+        [ pathScan "/answer/%d" (fun id -> deleteAnswer id) ]
+    ]
+    
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+
+  startWebServer defaultConfig app
+
+  printfn "Hello World from F#!"
+  0 // return an integer exit code
